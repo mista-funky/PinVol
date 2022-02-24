1b11407223a168cb2a61969710bfda9b63c27566
Mista-Funky <rcmarino@gmail.com>
2022-02-24 10:54:50 -0500

    Missed a few more refs to unused images

--- PinVol/Properties/Resources.Designer.cs
+++ PinVol/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace PinVol.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Chain {
-            get {
-                object obj = ResourceManager.GetObject("Chain", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Chain_Link_Sm {
             get {
                 object obj = ResourceManager.GetObject("Chain-Link-Sm", resourceCulture);
